1504f6218bacbea895217502c19f26aa21556d2f
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-03-04 20:34:52 +0100

    Black box

--- renderer.js
+++ renderer.js
@@ -1,7 +1,13 @@
 function Renderer() {
   this.canvas = document.createElement("canvas");
+  this.canvas.width = 600;
+  this.canvas.height = 400;
+
+  this.context = this.canvas.getContext("2d");
 }
 
 Renderer.prototype.render = function() {
   // console.log("rendering");
+  this.context.fillStyle = "#000";
+  this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 }
