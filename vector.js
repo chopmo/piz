288149560399b19495be10630cd528e6b1a2e14d
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-03-04 22:14:02 +0100

    Untested vector methods

--- vector.js
+++ vector.js
@@ -2,3 +2,16 @@ function Vector(x, y) {
   this.x = x;
   this.y = y;
 }
+
+Vector.prototype.getLength = function() {
+  Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+};
+
+Vector.prototype.divide = function(divident) {
+  this.x = this.x / divident;
+  this.y = this.y / divident;
+};
+
+Vector.prototype.normalize = function() {
+  this.divide(this.getLength());
+};
