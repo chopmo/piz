e7480df53ef8c162341aede6749f1fea4bf64d88
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-03-05 23:37:36 +0100

    Don't stop slow objects

--- drag.js
+++ drag.js
@@ -2,7 +2,6 @@ function Drag(circles, logger) {
   this.circles             = circles;
   this.logger              = logger;
   this.factor              = 0.03;
-  this.stopMotionThreshold = 0.05
 }
 
 Drag.prototype.update = function(timeDelta) {
@@ -17,10 +16,5 @@ Drag.prototype.applyDrag = function(circle) {
     return;
   }
 
-  if(velocity < this.stopMotionThreshold) {
-    this.logger.debug("Stopping motion.");
-    circle.velocity.reset();
-  } else {
-    circle.velocity.multiply(1 - this.factor);
-  }
+  circle.velocity.multiply(1 - this.factor);
 };
